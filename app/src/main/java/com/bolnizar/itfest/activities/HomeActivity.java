c11760711828814c4cbc7b8949b4a812e9953405
Boldijar Paul <paul.bv96@yahoo.com>
2016-11-26 21:48:24 +0200

    dome with coments + design fixes

--- app/src/main/java/com/bolnizar/itfest/activities/HomeActivity.java
+++ app/src/main/java/com/bolnizar/itfest/activities/HomeActivity.java
@@ -100,6 +100,7 @@ public class HomeActivity extends AppCompatActivity implements NextEventView {
             Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
+            finish();
         }
         return super.onOptionsItemSelected(item);
     }
