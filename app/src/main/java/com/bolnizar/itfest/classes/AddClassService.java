23a2bb4c957e0a7c745a18e33874193c75d21892
Boldijar Paul <paul.bv96@yahoo.com>
2016-11-26 17:54:19 +0200

    fixed issue, ading works

--- app/src/main/java/com/bolnizar/itfest/classes/AddClassService.java
+++ app/src/main/java/com/bolnizar/itfest/classes/AddClassService.java
@@ -11,7 +11,7 @@ import rx.Observable;
  */
 public interface AddClassService {
 
-    @GET("schools?order=name,asc")
+    @GET("classes?order=name,asc")
     Observable<SchoolsResponse> getSchools();
 
     @POST("schools")
